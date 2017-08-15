b9f47d3da236481dbf22a7df7287dda21f9d6c80
pkubis <pkubis@iterato.rs>
2017-08-15 16:46:54 +0200

    new webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,63 @@
+var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
     entry: ["./js/app.js"],
     output: {
-        filename: "js/out.js"
+        path: path.resolve('./build'),
+        filename: "bundle.js",
+        publicPath: './build/'
     },
+    plugins: [new webpack.optimize.UglifyJsPlugin()],
     watch: true,
     module: {
         loaders: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
+        },
+        {
+            test: /\.scss$/,
+            loaders: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+            test: /\.(png|jpg|gif|svg)$/,
+            loaders: [{
+                loader: 'file-loader',
+                options: {
+                    name: 'img/[name].[ext]',
+                    context: ''
                 }
             },
-
             {
-                test: /\.scss$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader']
+                loader: 'image-webpack-loader',
+                query: {
 
-            },
-            {
+                    mozjpeg: {
+                        progressive: true,
+                    },
+                    gifsicle: {
+                        interlaced: false,
+                    },
+                    optipng: {
+                        optimizationLevel: 5,
+                    },
+                    pngquant: {
+                        quality: '75-90',
+                        speed: 5,
+                    },
+                }
+            }
+            ]
+        },
 
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-                loader: 'url-loader'
-            },
+        {
+            test: /\.(eot|ttf|woff|woff2)$/,
+            loader: 'url-loader'
+        }
 
         ]
     }
